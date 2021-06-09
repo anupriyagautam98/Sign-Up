a0da8fa757ab94910e516aead1f4d0ed6e29a2f3
anupriyagautam98 <anupriya.gautam98@gmail.com>
2021-06-09 15:46:58 +0530

    Second

--- app.js
+++ app.js
@@ -19,16 +19,14 @@ app.post("/",function(req,res){
   const email=req.body.mail;
 
   const data={
-    members: [
-      {
+    members: [{
     email_address: email,
     status: "subscribed",
     merge_fields: {
       FNAME: firstname,
       LNAME: lastname
     }
-  }
-]
+  }]
 };
 
 const jsonData =JSON.stringify(data);
@@ -38,18 +36,21 @@ const options={
   auth: "anupriya1:b8f16b0de9dd6a96ea9b6014aa30e19d-us6"   //any name : api key
 }
 const request= https.request(url,options,function(response){
-
-  if(response.statuscode===200){
+  console.log(response.statusCode)
+  // details fill krr
+  if(response.statusCode===200){
     res.sendFile(__dirname+"/success.html");
   }else{
     res.sendFile(__dirname+"/fail.html");
   }
   response.on("data",function(data){
-    console.log(JSON.parse(data));
+    // console.log(JSON.parse(data));yayyyyyyyy
+    //ek spellling mistake ki kimat tm kya jano.........ashi babu hahahahhupppp
+    // 
   });
 })
 
-// request.write(jsonData);
+request.write(jsonData);
 request.end();
 });
 
